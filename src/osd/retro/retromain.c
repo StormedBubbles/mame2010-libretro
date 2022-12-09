116fc27b10bbe325060b5f163c65a190f6dfc38a
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-12-09 01:17:30 -0500

    Update retromain.c

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -1756,13 +1756,13 @@ void retro_poll_mame_input()
 	 }
 	 else
 	 {
-	    gun1Xs = 2 * gun1Xr;
+	    gun1Xs = 2 * gun1Xr + 26000;
 	    gun1Ys = 2 * gun1Yr;
 	 }
 
          if (jpark2x < 0)
 	 {
-	    gun2Xs = 2 * gun2Xr;
+	    gun2Xs = 2 * gun2Xr - 26000;
 	    gun2Ys = 2 * gun2Yr;
 	 }
 	 else
