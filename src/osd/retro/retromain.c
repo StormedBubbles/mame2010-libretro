74e6af1b46bfa7f528ce83034178d4d2091a9709
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-12-11 11:31:06 -0500

    Update retromain.c

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -1550,7 +1550,7 @@ void retro_poll_mame_input()
    int gun8Yr = input_state_cb(7, RETRO_DEVICE_LIGHTGUN, 0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_Y);
    int luckyx = 2 * gun2Xr - 13351;
    int jpark1x = 2 * gun1Xr + 5243;
-   int jpark2x = 2 * gun2Xr + 12838;
+   int jpark2x = 2 * gun2Xr - 4423;
    int gun1Xs, gun2Xs, gun3Xs, gun4Xs, gun5Xs, gun6Xs, gun7Xs, gun8Xs;
    int gun1Ys, gun2Ys, gun3Ys, gun4Ys, gun5Ys, gun6Ys, gun7Ys, gun8Ys;
 
