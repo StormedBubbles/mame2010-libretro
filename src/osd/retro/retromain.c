07e11abc5d33f61b10e10781c33195ef04cbe098
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-12-12 02:33:28 -0500

    Update retromain.c

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -1549,8 +1549,8 @@ void retro_poll_mame_input()
    int gun8Xr = input_state_cb(7, RETRO_DEVICE_LIGHTGUN, 0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_X);
    int gun8Yr = input_state_cb(7, RETRO_DEVICE_LIGHTGUN, 0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_Y);
    int luckyx = 2 * gun2Xr - 13351;
-   int jpark1x = 2 * gun1Xr + 5243;
-   int jpark2x = 2 * gun2Xr - 5243;
+   int jpark1x = 2 * gun1Xr + 6500;
+   int jpark2x = 2 * gun2Xr - 6500;
    int gun1Xs, gun2Xs, gun3Xs, gun4Xs, gun5Xs, gun6Xs, gun7Xs, gun8Xs;
    int gun1Ys, gun2Ys, gun3Ys, gun4Ys, gun5Ys, gun6Ys, gun7Ys, gun8Ys;
 
@@ -1751,7 +1751,7 @@ void retro_poll_mame_input()
       {
          if (jpark1x < 0)
 	 {
-	    gun1Xs = 2 * gun1Xr * 1.25 + 19500;
+	    gun1Xs = 2 * gun1Xr * 1.25 + 18000;
 	    gun1Ys = 2 * gun1Yr * 0.5;
 	 }
 	 else
@@ -1767,7 +1767,7 @@ void retro_poll_mame_input()
 	 }
 	 else
 	 {
-	    gun2Xs = 2 * gun2Xr * 1.25 - 18000;
+	    gun2Xs = 2 * gun2Xr * 1.25 - 16500;
 	    gun2Ys = 2 * gun2Yr * 0.5;
 	 }
 
